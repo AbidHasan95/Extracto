484b52a394a6f465785b7469270d8d6ad9b034d8
AbidHasan95 <sohailhsn@gmail.com>
2019-07-05 00:49:31 +0530

    pytesseract added and working

--- processPdf/views.py
+++ processPdf/views.py
@@ -1,49 +1,57 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadFileForm
-
+import pytesseract
+import os
+pytesseract.pytesseract.tesseract_cmd = r'A:\Tools\tesseract-ocr\tesseract'
 # Create your views here.
 
+
 def index(request):
-    #return HttpResponse("Pdf goes here")
-    #upload_file(request)
-    if(request.method=='POST'):
-        return upload_file(request)
-    return render(request, 'processPdf/home.html')
+    h = []
+    if(request.method == 'POST'):
+        h = upload_file(request)
+    return render(request, 'processPdf/home.html', {'content': h})
+
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
-        print(form.is_valid(),"Is is??")
+        print(form.is_valid(), "Is is??")
         if form.is_valid():
-            a =handle_uploaded_file(request.FILES['file'])
-            print("---a is ",a)
-            #return HttpResponseRedirect('/success/url/')
-            return HttpResponse("Successful!"+a)
+            a = handle_uploaded_file(request.FILES['file'])
+            print("---a is ", a)
+            return a
+            # return render(request, 'processPdf/home.html',{'content': a})
     else:
         form = UploadFileForm()
-    #return render(request, 'upload.html', {'form': form})
-    return HttpResponse("Success!!")
+    # return render(request, 'upload.html', {'form': form})
+    return "Failure"
+
 
 def handle_uploaded_file(file1):
-    print(type(file1),"hsga")
-    x = file1.read()
-    x= x.decode("utf-8")
-    print('The file',x,type(x))
-    
+    print(type(file1), "hsga")
+    #x = file1.read()
+    #x= x.decode("utf-8")
+    with open("media/"+file1.name, 'wb+') as f:
+        for chunk in file1.chunks():
+            f.write(chunk)
+        f.close()
+    filepath = "media/"+file1.name
+    print(f.name, "the file is this", filepath)
+    x = pytesseract.image_to_string(filepath)
+    os.remove(filepath)
+    #x = file1.name
+    print('The text', x, type(x))
+    x = x.split('\n')
+    print("The List is ", x)
     return x
-    # with open(file1) as file:
-    #     text = file.read()
-    #     print(text)
-    #     return text
 
 
-def save_pdf(pdf,filename):
+def save_pdf(pdf, filename):
     config = "--psm 4 --oem 1"
     #pdf = pytesseract.image_to_pdf_or_hocr("test.png",config=config,extension='pdf')
-    with open(filename,"wb") as file:
+    with open(filename, "wb") as file:
         file.write(bytearray(pdf))
+
         file.close()
-
-
-
